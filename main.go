f1dd2e647c68ff17f5c0443312814634dc8d937b
Nat Williams <nat.williams@amount.com>
2022-06-29 11:57:40 -0500

    sorta works, but how big is this field, actually?

--- main.go
+++ main.go
@@ -13,31 +13,10 @@ import (
 func main() {
 	app1 := app.New()
 
-	window1 := app1.NewWindow("Life!")
-
-	// game := NewGame()
-	// field := &CellField{Game: game}
+	window1 := app1.NewWindow("a triangle?")
 
 	field := NewTriangleField()
 
-	// pause button
-
-	// pauseButton := widget.NewButton("Pause", func() {
-	// 	game.TogglePause()
-	// })
-	// game.Paused.AddListener(binding.NewDataListener(func() {
-	// 	p, _ := game.Paused.Get()
-	// 	if p {
-	// 		pauseButton.SetText("Unpause")
-	// 	} else {
-	// 		pauseButton.SetText("Pause")
-	// 	}
-	// }))
-
-	// generation label
-
-	// genBoundText := binding.IntToStringWithFormat(game.Generation, "Generation: %d")
-	// generationText := widget.NewLabelWithData(genBoundText)
 	bottomText := widget.NewLabel("sup")
 
 	bottomToolbar := container.New(layout.NewGridLayout(2), bottomText)
@@ -49,8 +28,10 @@ func main() {
 	)
 
 	go func() {
-		for range time.Tick(time.Millisecond * 500) {
+		time.Sleep(time.Second)
+		for range time.Tick(time.Millisecond * 300) {
 			field.Tick()
+			field.Renderer.Refresh()
 		}
 	}()
 
@@ -58,4 +39,8 @@ func main() {
 	window1.Resize(fyne.NewSize(600, 600))
 	window1.SetFixedSize(true)
 	window1.ShowAndRun()
+}
+
+type Coord struct {
+	X, Y int
 }
--- triangle.go
+++ triangle.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"math/rand"
+	"sync"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -13,32 +13,92 @@ import (
 
 // TriangleField is a field for a triangle
 type TriangleField struct {
-	Points    map[Coord]bool
-	r         *rand.Rand
-	Vertices  []Coord
-	LastPoint Coord
+	Renderer   fyne.WidgetRenderer
+	Points     map[Coord]bool
+	pointMutex *sync.RWMutex
+	r          *rand.Rand
+	Vertices   []Coord
+	LastPoint  Coord
 	widget.BaseWidget
 }
 
+func (f *TriangleField) AddPoint(p Coord) {
+	f.pointMutex.Lock()
+	defer f.pointMutex.Unlock()
+	f.Points[p] = true
+}
+
+func (f *TriangleField) HasPoint(p Coord) bool {
+	// fmt.Println("in HasPoint")
+	// fmt.Println(f.pointMutex)
+	f.pointMutex.RLock()
+	defer f.pointMutex.RUnlock()
+	return f.Points[p]
+}
+
 func NewTriangleField() *TriangleField {
-	f := &TriangleField{}
-	f.Vertices = []Coord{
-		{300, 20},
-		{20, 580},
-		{580, 580},
+	f := &TriangleField{
+		Vertices: []Coord{
+			{600, 20},
+			{20, 1180},
+			{1180, 1180},
+		},
+		Points:     make(map[Coord]bool),
+		pointMutex: &sync.RWMutex{},
+		LastPoint:  Coord{600, 20},
+		r:          rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
-	f.Points = make(map[Coord]bool)
-	f.LastPoint = Coord{300, 20}
-	f.r = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 	return f
 }
 
 // CreateRenderer does something
 func (f *TriangleField) CreateRenderer() fyne.WidgetRenderer {
-	fmt.Println("what")
 	canvas := canvas.NewRasterWithPixels(
 		func(x, y, _, _ int) color.Color {
+			// c := Coord{x, y}
+			// if f.Points[c] {
+			// 	return color.Black
+			// }
+			// return color.White
+
+			vcs := []Coord{
+				{599, 19},
+				{599, 20},
+				{599, 21},
+				{600, 19},
+				{600, 20},
+				{600, 21},
+				{601, 19},
+				{601, 20},
+				{601, 21},
+
+				{19, 1179},
+				{19, 1180},
+				{19, 1181},
+				{20, 1179},
+				{20, 1180},
+				{20, 1181},
+				{21, 1179},
+				{21, 1180},
+				{21, 1181},
+
+				{1179, 1179},
+				{1180, 1180},
+				{1181, 1181},
+				{1179, 1179},
+				{1180, 1180},
+				{1181, 1181},
+				{1179, 1179},
+				{1180, 1180},
+				{1181, 1181},
+			}
+			for _, v := range vcs {
+				if v.X == x && v.Y == y {
+					return color.RGBA{0xff, 0x00, 0x00, 0xff}
+				}
+			}
+
 			toCheck := []Coord{
 				{x, y},
 				{x + 1, y},
@@ -51,14 +111,19 @@ func (f *TriangleField) CreateRenderer() fyne.WidgetRenderer {
 				{x, y - 1},
 			}
 			for _, c := range toCheck {
-				if f.Points[c] {
+				if f.HasPoint(c) {
 					return color.Black
 				}
 			}
 			return color.White
 		},
 	)
-	return widget.NewSimpleRenderer(canvas)
+	f.Renderer = widget.NewSimpleRenderer(canvas)
+	return f.Renderer
+}
+
+func (f *TriangleField) Refresh() {
+	f.Renderer.Refresh()
 }
 
 func (f *TriangleField) Tick() {
@@ -67,7 +132,6 @@ func (f *TriangleField) Tick() {
 	x := f.LastPoint.X + (target.X-f.LastPoint.X)/2
 	y := f.LastPoint.Y + (target.Y-f.LastPoint.Y)/2
 	c := Coord{x, y}
-	f.Points[c] = true
+	f.AddPoint(c)
 	f.LastPoint = c
-	fmt.Println(c)
 }
